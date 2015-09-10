f05fbf3ddebc164a5681874d02b6ca6c69bce58c
cnsasumi@gmail.com <cnsasumi@gmail.com>
2015-09-10 15:18:15 +0800

    change LIB_PATH to LITE_PATH

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,7 @@ if(version_compare(PHP_VERSION, '5.5.0') < 0){
 }
 
 !defined('DS') && define('DS', DIRECTORY_SEPARATOR);
-!defined('LIB_PATH') && define('LIB_PATH', __DIR__.DS.'src'.DS);
+!defined('LITE_PATH') && define('LITE_PATH', __DIR__.DS.'src'.DS);
 !defined('LITE_NS') && define('LITE_NS', 'Lite');
 
 //include loader
@@ -17,7 +17,7 @@ require_once __DIR__.'/src/junk/autoload.php';
 //注册自动加载库文件
 spl_autoload_register(function($className){
 	if(strpos($className, LITE_NS.'\\') === 0){
-		$file = str_replace(LITE_NS.'\\', LIB_PATH, $className);
+		$file = str_replace(LITE_NS.'\\', LITE_PATH, $className);
 		$file = str_replace('\\', DS, $file);
 		$file = $file.'.php';
 		if(is_file($file)){
