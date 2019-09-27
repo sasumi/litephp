0bd654fd3f2751873d464b0de67b12e9017d834a
sasumi <cnsasumi@gmail.com>
2019-09-27 10:03:28 +0800

    get upload size in human readable option

--- src/Component/Server.php
+++ src/Component/Server.php
@@ -1,5 +1,6 @@
 <?php
 namespace Lite\Component;
+use function Lite\func\format_size;
 use function Lite\func\resolve_size;
 
 /**
@@ -14,13 +15,18 @@ class Server{
 	/**
 	 * 服务器最大上传文件大小
 	 * 通过对比文件上传限制与post大小获取
+	 * @param bool $human_readable 是否以可读方式返回
 	 * @return int
 	 */
-	public static function getUploadMaxSize(){
+	public static function getUploadMaxSize($human_readable = false){
 		$upload_sz = trim(ini_get('upload_max_filesize'));
 		$upload_sz = resolve_size($upload_sz);
 		$post_sz = trim(ini_get('post_max_size'));
 		$post_sz = resolve_size($post_sz);
-		return min($upload_sz, $post_sz);
+		$ret = min($upload_sz, $post_sz);
+		if($human_readable){
+			return format_size($ret);
+		}
+		return $ret;
 	}
 }
