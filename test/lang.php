2ae5cb4b6e7bd41581a98129fee5370240867122
sasumi <cnsasumi@gmail.com>
2019-10-08 22:47:53 +0800

    test lang

--- test/lang.php
+++ test/lang.php
@@ -5,6 +5,18 @@
  * Date: 2019/06/20
  * Time: 09:24
  */
+
+use function Lite\func\t;
+use Lite\I18N\Lang as Lang;
+
 include '../bootstrap.php';
-$lang = \Lite\I18N\Lang::instance()->detectLanguageFromBrowser();
-dump($lang, 1);
+Lang::setSupportLanguageList(['zh_CN', 'en_US']);
+Lang::bindDomain(Lang::DOMAIN_DEFAULT, __DIR__);
+Lang::bindDomain('menu', __DIR__);
+$lang = $_SESSION['lang'] ?: Lang::detectLanguageListFromBrowser()[0] ?: 'zh_CN';
+$lang = 'en_US';
+Lang::setCurrentLanguage($lang);
+
+t('hello world');
+
+dump(Lang::getCurrentLanguage(), 1);
